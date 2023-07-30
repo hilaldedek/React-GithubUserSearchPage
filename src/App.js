eea3d52d538e25ce47d005c11b0476539c49d84c
hilal dedek <hdedek03@gmail.com>
2023-07-30 13:23:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Avatar, Button, TextField } from '@mui/material';
+import { useState } from 'react';
 
 function App() {
+  const[input,setInput]=useState("");
+  const[user,setUser]=useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const getUser=async function(){
+      const url=`https://api.github.com/users/${input}`;
+      const res= await fetch(url);
+      const data=await res.json();
+      setUser(data)
+      console.log(data);
+    }
+    getUser();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" value={input} onChange={(e)=>setInput(e.target.value)}/>
+        <Button type='submit'>Search</Button>
+      </form>
+      <Avatar
+  alt="Remy Sharp"
+  src={user?.avatar_url}
+  sx={{ width: 200, height: 200 }}
+/>
+
+      
+
+      <h1>{user?.bio}</h1>
     </div>
   );
 }
