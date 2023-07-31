8d0ddc23290b5fabbbca965ba0aa9bbc2d3f90af
hilal dedek <hdedek03@gmail.com>
2023-07-31 12:53:15 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Avatar, Button, Link, TextField } from '@mui/material';
+import { Avatar, Button,TextField} from '@mui/material';
 import { useState } from 'react';
 
 function App() {
@@ -16,25 +16,38 @@ function App() {
       console.log(data);
     }
     getUser();
+  
   }
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" value={input} onChange={(e)=>setInput(e.target.value)}/>
-        <Button type='submit'>Search</Button>
+    <div className="App" style={{backgroundColor:"#331D2C",color:'#EDE4FF',fontFamily:"Raleway"}}>
+      <form onSubmit={handleSubmit} style={{display:'flex',flexDirection:'column',marginTop:24}}>
+        <TextField value={input} placeholder='Username' onChange={(e)=>setInput(e.target.value)} style={{width:'50%',position:'relative',backgroundColor:"#A78295",margin:'auto'}} />
+        <div style={{marginTop:24}} >
+          <Button type='submit' style={{width:"25%",position:'relative',margin:'auto',backgroundColor:"#A78295",color:'#EDE4FF'}}>Search</Button>
+        </div>
+        
       </form>
-      <Avatar
+      {
+        user.message==="Not Found" ? <h1 style={{textShadow:" 6px 6px 0px rgba(0,0,0,0.8)"}}>User not found</h1> : ""
+        
+      }
+      <h1 style={{color:'#EDE4FF',fontFamily:"Raleway",fontSize:'4rem',textShadow:" 6px 6px 0px rgba(0,0,0,0.8)"}}>{user?.name}</h1>
+      {
+        user.avatar_url && <Avatar
   alt="Remy Sharp"
-  src={user?.avatar_url}
+  src={user.avatar_url} style={{position:'relative',margin:'auto'}}
   sx={{ width: 200, height: 200 }}
 />
-<Link href={user?.blog} variant="body2">
+      }
+      {/* {
+        user.blog &&<Link href={user?.blog} target="_blank">
   Linkedin
 </Link>
-{/* <Textarea>Followers : {user?.followers}</Text> */}
-      
+      } */}
+      <h1 style={{textShadow:" 6px 6px 0px rgba(0,0,0,0.8)"}}>{user.bio}</h1 >
+     { user.followers && <h3 style={{textShadow:" 6px 6px 0px rgba(0,0,0,0.8)"}}>{user.followers} followers</h3>}
+      {user.following && <h3 style={{textShadow:" 6px 6px 0px rgba(0,0,0,0.8)"}}>{user.following} following</h3>}
 
-      <h1>{user?.bio}</h1>
     </div>
   );
 }
